60170767c8df3b27b6dfad73f624a5ce2f3ab2f1
Arquetipo28 <german.reynagaa@gmail.com>
2021-06-07 19:59:03 -0500

    chore: modify configuration to work with API client

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'test', action: :test, controller: :test
+
+  post '/links', action: :create, controller: :links
+  get '/top', action: :top, controller: :links
+  put '/update_count', action: :update_count, controller: :links
 end
